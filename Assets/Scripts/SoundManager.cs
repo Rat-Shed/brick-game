7d117030b4a6abe768aa981c69139aea4ba18e3f
Oscar J <oscarjemmott@gmail.com>
2025-07-14 15:29:13 +1000

    added jump sound, tweaked step sounds

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -9,12 +9,15 @@ public class SoundManager : MonoBehaviour
 
     private EventInstance brickStepInstance;
     [SerializeField] private EventReference _brickStepReference;
+    private EventInstance brickJumpInstance;
+    [SerializeField] private EventReference _brickJumpReference;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         brickPlaceInstance = RuntimeManager.CreateInstance(_brickPlaceReference);
         brickStepInstance = RuntimeManager.CreateInstance(_brickStepReference);
+        brickJumpInstance = RuntimeManager.CreateInstance(_brickJumpReference);
     }
 
     public void SoundBrickPlace()
@@ -25,5 +28,9 @@ public class SoundManager : MonoBehaviour
     public void SoundBrickStep()
     {
         brickStepInstance.start();
+    }
+    public void SoundBrickJump()
+    {
+        brickJumpInstance.start();
     }
 }
